f9ecb22b13fd56e93a1c5c58f03a269d4f8eb6c9
maida-fl <maida.guzman@outlook.com>
2021-09-06 20:56:37 -0300

    procfile y configuracion para heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto http://localhost:3000");
 });
 
